8460ab66cd17589a5063d35aeb42800b26eff091
chuongite <simane161@gmail.com>
2025-11-18 23:18:54 +0700

    Submit bai tap Chuyen doi tien te

---
+++ money-exchange/src/MoneyExchange.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.Scanner;
+
+public class MoneyExchange {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Moi ban nhap so tien USD: ");
+        usd = sc.nextDouble();
+        double quydoi = usd * vnd;
+        System.out.print("Gia tri VND: " + quydoi);
+    }
+}
